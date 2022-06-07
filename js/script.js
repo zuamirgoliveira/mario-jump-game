c54b384a5f5ec62fca077c7acd6447b4385b4195
zuamirgoliveira <zuamir.g.oliveira@accenture.com>
2022-06-06 22:55:03 -0300

    feat[] Implements base mario game actions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
-
     }, 500);
 
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    if (pipePosition <= 35 && pipePosition > 0 && marioPosition < 64) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/mario-game-over.png';
+
+        setTimeout(() => {
+            mario.classList.add('gameover');
+
+        }, 500);
+
+        clearInterval(loop);
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);
